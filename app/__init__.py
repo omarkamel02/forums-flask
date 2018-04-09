031fd76f170148a090caef5bcd7341314638ff3e
Omar Kamel <omarko.kamel@gmail.com>
2018-04-09 15:49:14 +0300

    fixed views importing

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 member_store = stores.MemberStore()
 post_store = stores.PostsStore()
 dummy_data.seed_stores(member_store, post_store)
-from views import*
+#from views import* for local
+from app import views
